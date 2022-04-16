eaaad94812bf5200c073a53f20c0e96099e31764
Alvin Zhao <14051545+yzAlvin@users.noreply.github.com>
2022-04-17 00:54:56 +1000

    add my heroku url

--- next.config.js
+++ next.config.js
@@ -5,6 +5,6 @@ const nextConfig = {
 
 const production = process.env.NODE_ENV === "production";
 
-const url = production ? "https://www.yoursite.com" : "http://localhost:1337";
+const url = production ? "https://yzalvin-cms.herokuapp.com" : "http://localhost:1337";
 
 module.exports = { nextConfig, url };
