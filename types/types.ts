c7a4dfdc5ac6f31eb3d1d3a90d7d367d2a2295af
Alvin Zhao <14051545+yzAlvin@users.noreply.github.com>
2022-04-16 23:53:35 +1000

    add about page

--- types/types.ts
+++ types/types.ts
@@ -10,3 +10,15 @@ export type ArticleAttributes = {
     updatedAt: string;
     publishedAt: string;
 }
+
+export type AboutMe = {
+    id: string;
+    attributes: AboutMeAttributes;
+}
+
+export type AboutMeAttributes = {
+    Body: string;
+    createdAt: string;
+    updatedAt: string;
+    publishedAt: string;
+}
