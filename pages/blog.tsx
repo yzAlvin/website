bff85376095fee687d25a9ce4fe5d939eb610329
Alvin Zhao <14051545+yzAlvin@users.noreply.github.com>
2022-04-18 04:20:00 +1000

    add blog page

---
+++ pages/blog.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import {fetchData} from "../utils";
+import {Article} from "../types/types";
+import {InferGetStaticPropsType} from "next";
+
+export const getStaticProps = async () => {
+    const list = await fetchData<Article[]>("articles")
+
+    return {
+        props: {
+            list,
+        }, revalidate: 1,
+    };
+};
+
+const Blog = ({list}: InferGetStaticPropsType<typeof getStaticProps>) => {
+    return (<>
+        <h1>Blog</h1>
+        <ul>
+            {list.map((item) => (<li key={item.id}>
+                <Link href={`/article/${item.id}`}>
+                    <a>{item.attributes.title}</a>
+                </Link>
+            </li>))}
+        </ul>
+    </>)
+}
+
+export default Blog;
