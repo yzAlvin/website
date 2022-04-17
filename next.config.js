03b94bc26ffc4da7380488eb47479c46d9a3286e
Alvin Zhao <14051545+yzAlvin@users.noreply.github.com>
2022-04-18 04:54:48 +1000

    add cloudinary to domain in config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
 };
 
 const production = process.env.NODE_ENV === "production";
 
 const url = production ? "https://yzalvin-cms.herokuapp.com" : "http://localhost:1337";
 
-module.exports = { nextConfig, url };
+module.exports = {
+    nextConfig, url, images: {
+        domains: ['res.cloudinary.com/alvinzhao/'],
+    },
+};
