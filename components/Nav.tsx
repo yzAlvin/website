c22ed7afa1d7442741c7a4faa84423fa060ce119
Alvin Zhao <14051545+yzAlvin@users.noreply.github.com>
2022-04-18 04:19:56 +1000

    add nav

---
+++ components/Nav.tsx
@@ -0,0 +1,8 @@
+export default function Nav() {
+    return (
+        <nav>
+            <a href="/blog/">Blog</a>
+            <a href="/projects/">Projects</a>
+        </nav>
+    );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,40 +2,32 @@ import type {InferGetStaticPropsType, NextPage} from "next";
 import Head from "next/head";
 import Link from "next/link";
 import {AboutMe, Article} from "../types/types";
-import About from "../components/about";
+import About from "../components/About";
 import {fetchData} from "../utils";
+import Nav from "../components/Nav";
 
 export const getStaticProps = async () => {
-    const list = await fetchData<Article[]>("articles")
     const about = await fetchData<AboutMe>("about-me")
 
   return {
     props: {
-      list,
         about,
     },
     revalidate: 1,
   };
 };
 
-const Home = ({ list, about }: InferGetStaticPropsType<typeof getStaticProps>) => {
+const Home = ({ about }: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <div>
       <Head>
         <title>Alvin Zhao</title>
       </Head>
+        <header>
+            <Nav/>
+        </header>
       <h1>Alvin Zhao</h1>
        <About data={about}/>
-        <h2>Blog Posts</h2>
-      <ul>
-        {list.map((item) => (
-          <li key={item.id}>
-            <Link href={`/article/${item.id}`}>
-              <a>{item.attributes.title}</a>
-            </Link>
-          </li>
-        ))}
-      </ul>
     </div>
   );
 };
