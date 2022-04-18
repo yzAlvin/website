923f540612b95cddd1bcd2083a59d24cf6f0c769
Alvin Zhao <14051545+yzAlvin@users.noreply.github.com>
2022-04-19 00:33:21 +1000

    refactor project page

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -6,6 +6,7 @@ import Image from 'next/image'
 import Nav from "../components/Nav";
 import ReactMarkdown from "react-markdown";
 import Tag from "../components/Tag";
+import {FiExternalLink, FiGithub} from "react-icons/fi";
 
 export const getStaticProps = async () => {
     const list = await fetchData<Project[]>("projects?populate=*")
@@ -17,32 +18,42 @@ export const getStaticProps = async () => {
     };
 };
 
-const buildUrl = (project: Project, width: Number, height: Number) =>
-    `https://res.cloudinary.com/alvinzhao/image/upload/w_${width},h_${height},c_fill/${project.attributes.cover?.data.attributes.hash}`
+const buildUrl = (project: Project, width: Number, height: Number) => `https://res.cloudinary.com/alvinzhao/image/upload/w_${width},h_${height},c_fill/${project.attributes.cover?.data.attributes.hash}`
 
 const Projects = ({list}: InferGetStaticPropsType<typeof getStaticProps>) => {
     return (<>
         <Nav/>
         <h1 className="has-text-light title is-1 has-text-centered">Projects</h1>
-        {list.map((item) => (<div className="card blog-post-card has-background-grey-darker mb-5 p-3" key={item.id}>
-            <Link href={`/project/${item.attributes.slug}`}>
-                <a>
-                    <div className="columns">
-                        <div className="column">
-                    {item.attributes.cover?.data?.attributes.url && <Image width={500} height={150}
-                                                                            src={buildUrl(item, 500, 150)}
-                                                                           alt={item.attributes.cover.data.attributes.alternativeText}/>}
-                        </div>
-                        <div className="column">
-                    <p className="title is-5 has-text-light">{item.attributes.title}</p>
-                    <ReactMarkdown className="subtitle is-6 has-text-light markdown-body">{item.attributes.description}</ReactMarkdown>
-                    {item.attributes.tags.data && item.attributes.tags.data.map((tag) => <Tag key={tag.id} {...tag}/>)}
-                        </div>
-                    </div>
-                </a>
-            </Link>
-        </div>))}
+        {list.map((item) => <Project key={item.id} {...item}/>)}
     </>)
 }
+
+const Project = (item: Project) => <div className="card blog-post-card has-background-grey-darker mb-5 p-3">
+    <Link href={`/project/${item.attributes.slug}`}>
+        <a>
+            <div className="columns">
+                <div className="column">
+                    {item.attributes.cover?.data?.attributes.url && <Image width={500} height={170}
+                                                                           src={buildUrl(item, 500, 170)}
+                                                                           alt={item.attributes.cover.data.attributes.alternativeText}/>}
+                </div>
+                <div className="column">
+                    <h3 className="title is-5 has-text-light mb-3">{item.attributes.title}</h3>
+                    <div className="project-links">
+                        {item.attributes.repoLink &&
+                            <a href={item.attributes.repoLink} target="_blank" rel="noreferrer"><FiGithub
+                                size={20}/></a>}
+                        {item.attributes.projectLink && <a href={item.attributes.projectLink} target="_blank"
+                                                           rel="noreferrer"><FiExternalLink size={20}/></a>}
+                    </div>
+                    <ReactMarkdown
+                        className="subtitle is-6 has-text-light markdown-body mb-3">{item.attributes.description}</ReactMarkdown>
+                    {item.attributes.tags.data && item.attributes.tags.data.map((tag) => <Tag
+                        key={tag.id} {...tag}/>)}
+                </div>
+            </div>
+        </a>
+    </Link>
+</div>
 
 export default Projects;
