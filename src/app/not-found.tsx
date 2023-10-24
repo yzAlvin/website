c629d278f6c0fa4da4a9b2bd94abbc1f0dad7961
Alvin Zhao <14051545+yzAlvin@users.noreply.github.com>
2023-10-24 23:21:26 +1100

    Update not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,12 @@
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>There is nothing here...</p>
+    <div className="h-screen flex items-center justify-center text-center">
+      <div>
+        <h2 className="text-red-300">Not Found</h2>
+        <br/>
+        <p>If you got here, maybe this page route is still WIP 🚧</p>
+        <p>Or maybe there is nothing here... Did you try to guess a URL? 🙈🙈</p>
+      </div>
     </div>
   )
 }
