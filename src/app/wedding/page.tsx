8d5eed7f8b53f7523ea3cb79fce240e88aa1b759
Alvin Zhao <14051545+yzAlvin@users.noreply.github.com>
2024-04-26 10:41:32 +1000

    no '

--- src/app/wedding/page.tsx
+++ src/app/wedding/page.tsx
@@ -1,7 +1,7 @@
 export default function WeddingPage() {
     return (
         <div className="m-8 text-center items-center">
-            <h1 className="mb-8 text-4xl font-bold">Alvin and Joy's Wedding</h1>
+            <h1 className="mb-8 text-4xl font-bold">Alvin and Joys Wedding</h1>
             <a href="http://google.com">
             <div className="border rounded-lg w-1/3 mx-auto mb-4 p-2 hover:bg-blue-800">
                 Photos 
