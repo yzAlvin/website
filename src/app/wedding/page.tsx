09736ed409444a189484aae6dc8661b67cae7aa0
Alvin Zhao <14051545+yzAlvin@users.noreply.github.com>
2024-05-07 21:53:40 +1000

    Extract to carousel component..

--- src/app/wedding/page.tsx
+++ src/app/wedding/page.tsx
@@ -1,27 +1,11 @@
+import Carousel from "../../components/Carousel"
+
 export default function WeddingPage() {
     return (
         <div className="h-screen w-screen flex items-center justify-center">
             <div className="absolute bg-scroll bg-cover bg-[url('/couple.jpeg')] h-screen w-screen blur grayscale">
             </div>
-            <div className="rounded-xl rounded bg-white relative text-center p-4 flex-col lg:w-1/2 w-10/12">
-                <img src='/couple.jpeg' alt={'photo'} className="rounded-lg grayscale drop-shadow-2xl h-96 w-full object-cover" />
-                <div className="mt-8" >
-                    <h2 className="mb-4 font-bold text-xl text-slate-900">Photo Share</h2>
-
-                    <p className="mb-6 px-4 text-slate-800">We are so happy you are celebrating with us and we would love to see your lovely faces!</p>
-                    
-                    <a href="http://google.com">
-                        <div className="border rounded-lg xl:w-1/3 w-2/3 mx-auto p-2 text-slate-900 transition bg-gray-200 hover:bg-gray-400">
-                            Upload Your Photos!
-                        </div>
-                    </a>
-                </div>
-                <div className="flex items-center justify-center gap-3 mt-8">
-                    <svg className="h-2 fill-black" xmlns="http://www.w3.org/2000/svg" viewBox="7.8 7.8 4.4 4.4"><path d="M7.8 10a2.2 2.2 0 0 0 4.4 0 2.2 2.2 0 0 0-4.4 0z"/></svg>
-                    <svg className="h-2 fill-slate-400" xmlns="http://www.w3.org/2000/svg" viewBox="7.8 7.8 4.4 4.4"><path d="M7.8 10a2.2 2.2 0 0 0 4.4 0 2.2 2.2 0 0 0-4.4 0z"/></svg>
-                    <svg className="h-2 fill-slate-400" xmlns="http://www.w3.org/2000/svg" viewBox="7.8 7.8 4.4 4.4"><path d="M7.8 10a2.2 2.2 0 0 0 4.4 0 2.2 2.2 0 0 0-4.4 0z"/></svg>
-                </div>
-            </div>
+            <Carousel />
         </div>
     )
 }
