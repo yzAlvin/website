b90e878fc49737e480be92aec20bb1af5d61ae9a
Alvin Zhao <14051545+yzAlvin@users.noreply.github.com>
2024-05-07 21:04:18 +1000

    Style like the design..

--- src/app/wedding/page.tsx
+++ src/app/wedding/page.tsx
@@ -1,22 +1,22 @@
 export default function WeddingPage() {
     return (
-        <div className="m-8 text-center items-center">
-            <h1 className="mb-8 text-4xl font-bold">Alvin and Joys Wedding</h1>
-            <a href="http://google.com">
-            <div className="border rounded-lg w-1/3 mx-auto mb-4 p-2 hover:bg-blue-800">
-                Photos 
+        <div className="h-screen w-screen flex items-center justify-center">
+            <div className="absolute bg-scroll bg-cover bg-[url('/couple.jpeg')] h-screen w-screen blur grayscale">
             </div>
-            </a>
-            <a href="http://google.com">
-            <div className="border rounded-lg w-1/3 mx-auto mb-4 p-2 hover:bg-red-800">
-                Crossword
+            <div className="rounded-xl rounded bg-white relative text-center p-4 flex-col lg:w-1/2 w-10/12">
+                <img src='/couple.jpeg' alt={'photo'} className="rounded-lg grayscale drop-shadow-2xl h-96 w-full object-cover" />
+                <div className="mt-8" >
+                    <h2 className="mb-8 font-bold text-xl text-slate-900">Photo Share</h2>
+
+                    <p className="mb-8 px-4 text-slate-800">We are so happy you are celebrating with us and we would love to see your lovely faces!</p>
+                    
+                    <a href="http://google.com">
+                        <div className="border rounded-lg xl:w-1/3 w-2/3 mx-auto mb-4 p-2 text-slate-900 transition bg-gray-300 hover:bg-gray-400">
+                            Upload Your Photos!
+                        </div>
+                    </a>
+                </div>
             </div>
-            </a>
-            <a href="http://google.com">
-            <div className="border rounded-lg w-1/3 mx-auto p-2 hover:bg-green-800">
-                Some other thing
-            </div>
-            </a>
         </div>
     )
 }
