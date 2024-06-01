aa9174b73380d17512fa4b5a5de59b6fe4c3340a
Alvin Zhao <14051545+yzAlvin@users.noreply.github.com>
2024-06-02 03:50:04 +1000

    Homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       {/* <Nav /> */}
-      <main className="text-zinc-100">
+      <main className="text-zinc-100 flex justify-center">
         {/* <p>Heroku's free tier shut down so I stopped hosting the previous backend</p>
         <p>This is a rebuild but now using server side generation instead of the client fetching</p>
         <p>BUT I don't have time to work on this at the moment so just message me if you want to see my projects</p> */}
--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,15 +1,37 @@
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
-import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function About() {
   return (
-    <>
-      <h1>Alvin Zhao</h1>
+    <div className="text-center m-10 space-y-7">
+      <h1 className="text-3xl font-bold">Alvin Zhao</h1>
 
-      <p>Currently digitising eye care @ iCare</p>
+      <div className="flex justify-center">
+        <img src="me.jpg" alt="me" width={150} height={150} className="rounded-full"/>
+      </div>
 
-      {/* <div className="flex flex-row items-center gap-8">
+      <div>
+        <h2 className="text-lg font-semibold">Current</h2>
+
+        <ul className="list-disc list-inside text-left space-y-1">
+          <li>Digitising eye care @ <a className="text-blue-500 underline" href="https://www.icare-world.com/">iCare</a></li>
+          <li>Director @ <a className="text-blue-500 underline" href="https://www.berrytherapy.com.au/">Berry Therapy</a></li>
+          <li>Consulting @ 021.au</li>
+        </ul>
+      </div>
+
+      <div>
+        <h2 className="text-lg font-semibold">Past</h2>
+
+        <ul className="list-disc list-inside text-left space-y-1 [&_ul]:list-[revert]">
+          <li>Early Engineer @ <a className="text-blue-500 underline" href="https://www.lyrebirdhealth.com/">Lyrebird Health</a></li>
+          <li>Mentor & Contributor on <a className="text-blue-500 underline" href="https://exercism.org/">Exercism</a></li>
+          <li>Platform Engineer @ <a className="text-blue-500 underline" href="https://www.myob.com/">MYOB</a></li>
+        </ul>
+      </div>
+
+      <div className="flex flex-row justify-center items-center gap-8 pt-10">
         <a className="hover:text-violet-400" href={"https://github.com/yzalvin/"} target="_blank" rel="noreferrer">
           <FontAwesomeIcon icon={faGithub} width="50" />
         </a>
@@ -19,7 +41,7 @@ export default function About() {
         <a className="hover:text-violet-400" href={"mailto:yzalvin@duck.com"} target="_blank" rel="noreferrer">
           <FontAwesomeIcon icon={faEnvelope} width="50" />
         </a>
-      </div> */}
-    </>
-  )
+      </div>
+    </div>
+  );
 }
