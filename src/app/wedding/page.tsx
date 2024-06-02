cba6b4753bae99826b15553636489c20300c7d7a
Alvin Zhao <14051545+yzAlvin@users.noreply.github.com>
2024-06-02 20:58:33 +1000

    Constrain the DialogContent so phone doesn't break

--- src/app/wedding/page.tsx
+++ src/app/wedding/page.tsx
@@ -3,7 +3,7 @@ import Carousel from "@/components/Carousel"
 
 export default function WeddingPage() {
     return (
-        <div className="h-screen w-screen flex flex-col items-center justify-center">
+        <div className="h-screen w-screen flex flex-col items-center justify-center min-h-screen">
             <div className="absolute bg-scroll bg- bg-repeat bg-[url('/couple.jpeg')] h-full w-full blur grayscale">
             </div>
             <Carousel />
