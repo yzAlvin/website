dce63243590ecfa6982868665bab1969bd21b7ee
Alvin Zhao <14051545+yzAlvin@users.noreply.github.com>
2024-06-02 21:13:31 +1000

    Try fix wedding page margins

--- src/app/wedding/page.tsx
+++ src/app/wedding/page.tsx
@@ -4,7 +4,7 @@ import Carousel from "@/components/Carousel"
 export default function WeddingPage() {
     return (
         <div className="h-screen w-screen flex flex-col items-center justify-center min-h-screen">
-            <div className="absolute bg-scroll bg- bg-repeat bg-[url('/couple.jpeg')] h-full w-full blur grayscale">
+            <div className="absolute bg-scroll bg-repeat bg-[url('/couple.jpeg')] min-h-screen w-full blur grayscale">
             </div>
             <Carousel />
             <Footer />
