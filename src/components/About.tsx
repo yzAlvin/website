5dc9d88f95c35dc7131493671fb9fc713564569d
Alvin Zhao <14051545+yzAlvin@users.noreply.github.com>
2024-06-02 21:13:56 +1000

    Add target _blank to all links

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -15,8 +15,8 @@ export default function About() {
         <h2 className="text-lg font-semibold">Current</h2>
 
         <ul className="list-disc list-inside text-left space-y-1">
-          <li>Digitising eye care @ <a className="text-blue-500 underline" href="https://www.icare-world.com/">iCare</a></li>
-          <li>Director @ <a className="text-blue-500 underline" href="https://www.berrytherapy.com.au/">Berry Therapy</a></li>
+          <li>Digitising eye care @ <a className="text-blue-500 underline" target="_blank" href="https://www.icare-world.com/">iCare</a></li>
+          <li>Director @ <a className="text-blue-500 underline" target="_blank" href="https://www.berrytherapy.com.au/">Berry Therapy</a></li>
           <li>Consulting @ 021.au</li>
         </ul>
       </div>
@@ -25,9 +25,9 @@ export default function About() {
         <h2 className="text-lg font-semibold">Past</h2>
 
         <ul className="list-disc list-inside text-left space-y-1 [&_ul]:list-[revert]">
-          <li>Early Engineer @ <a className="text-blue-500 underline" href="https://www.lyrebirdhealth.com/">Lyrebird Health</a></li>
-          <li>Mentor & Contributor on <a className="text-blue-500 underline" href="https://exercism.org/">Exercism</a></li>
-          <li>Platform Engineer @ <a className="text-blue-500 underline" href="https://www.myob.com/">MYOB</a></li>
+          <li>Early Engineer @ <a className="text-blue-500 underline" target="_blank" href="https://www.lyrebirdhealth.com/">Lyrebird Health</a></li>
+          <li>Mentor & Contributor on <a className="text-blue-500 underline" target="_blank" href="https://exercism.org/">Exercism</a></li>
+          <li>Platform Engineer @ <a className="text-blue-500 underline" target="_blank" href="https://www.myob.com/">MYOB</a></li>
         </ul>
       </div>
 
